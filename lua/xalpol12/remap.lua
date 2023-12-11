92b53099b6e1780571cb46e0e3d0e76ab62b6e85
xalpol12 <xalpol12@gmail.com>
2023-12-11 20:58:50 +0100

    add some new keybindings with descriptions

--- lua/xalpol12/remap.lua
+++ lua/xalpol12/remap.lua
@@ -1,6 +1,28 @@
-vim.g.mapleader = " "
+-- open netrw keymap
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- moving whole blocks of code when in visual line mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- cursor stays in place when appending line below to the current line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- half-page jumping keeps cursor in the middle of the screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- search highlighting jump keeps cursor in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- replace select without losing paste current paste buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- copy to global system buffer
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
+
 

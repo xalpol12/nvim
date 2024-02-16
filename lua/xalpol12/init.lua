511a992b28d5425242485ff0d550e91840ebc799
Dawid Stawiszyński <xalpol12@wp.pl>
2024-02-16 18:45:04 +0100

    fix packer setup after fresh clone

--- lua/xalpol12/init.lua
+++ lua/xalpol12/init.lua
@@ -1,2 +1,3 @@
+require("xalpol12.packer")
 require("xalpol12.remap")
 require("xalpol12.set")
--- lua/xalpol12/packer.lua
+++ lua/xalpol12/packer.lua
@@ -1,5 +1,18 @@
+-- bootstrap
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+      fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+      vim.cmd [[packadd packer.nvim]]
+      return true
+    end
+    return false
+  end
+  
+  local packer_bootstrap = ensure_packer()
+
 -- packer setup 
-vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
@@ -61,4 +74,8 @@ return require('packer').startup(function(use)
       }
   }
 
+  -- auto setup after cloning config
+    if packer_bootstrap then
+        require('packer').sync()
+    end
   end)
